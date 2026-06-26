a02aaf2ac7d673f3a99b6eef117c8a366796e13e
Deixtexx <serebrovdaniil@gmail.com>
2026-06-26 22:33:34 +0600

    задание 1

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+        input = input.ToLower();
+        input = new string(input.Where(c => !Char.IsPunctuation(c) && !Char.IsWhiteSpace(c)).ToArray());
+        return input.Equals(new string(input.Reverse().ToArray()));
+    }
+}
