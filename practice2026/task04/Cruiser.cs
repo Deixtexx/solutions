bed62fff175b96b12f0866ab582549fa44636bc0
Deixtexx <serebrovdaniil@gmail.com>
2026-07-06 18:13:34 +0600

    названия файлов, конфигурация для сборки

---
+++ practice2026/task04/Cruiser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed { get; private set; }
+        public int FirePower { get; private set; }
+        public int HP { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
+        public double Angle { get; private set; } // в радианах
+        public Cruiser(double posX, double posY)
+        {
+            Speed = 50;
+            FirePower = 100;
+            HP = 200;
+            PositionX = posX;
+            PositionY = posY;
+        }
+        public void MoveForward()
+        {
+            PositionX += Speed * Math.Cos(Angle);
+            PositionY += Speed * Math.Sin(Angle);
+        }
+
+        // представим, что задается новый угол
+        public void Rotate(int angle)
+        {
+            Angle = (double)angle * (Math.PI / 180.0);
+        }
+
+        public void Fire(ISpaceship target)
+        {
+            target.HP -= FirePower;
+        }
+    }
+}
---
+++ practice2026/task04/ISpaceship.cs
@@ -0,0 +1,12 @@
+﻿namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();      // Движение вперед
+        void Rotate(int angle);  // Поворот на угол (градусы)
+        void Fire(ISpaceship target); // Выстрел ракетой по врагу
+        int Speed { get; }       // Скорость корабля
+        int FirePower { get; }   // Мощность выстрела
+        int HP { get; set; }     // Показатель здоровья
+    }
+}
