f60d125c20e6b8f85ee0796d3f58bd9f58577c9c
Deixtexx <serebrovdaniil@gmail.com>
2026-07-11 21:44:56 +0600

    задание 13

---
+++ practice2026_3/task13/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace task13
+{
+    public class Student
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime BirthDate { get; set; }
+        public List<Subject>? Grades { get; set; }
+    }
+}
