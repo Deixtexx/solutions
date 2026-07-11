f69814ed18a16b89879c2a04308826f88243d122
Deixtexx <serebrovdaniil@gmail.com>
2026-07-11 18:10:01 +0600

    задание 10

---
+++ practice2026_2/task10/AssemblyLoadContext.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Runtime.Loader;
+using System.Reflection;
+
+namespace task10
+{
+    public class PluginLoadContext : AssemblyLoadContext
+    {
+        private readonly string _pluginFolder;
+
+        public PluginLoadContext(string pluginFolder) : base(isCollectible: true)
+        {
+            _pluginFolder = pluginFolder;
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            // Проверяем, есть ли зависимая сборка в папке плагинов
+            string assemblyPath = Path.Combine(_pluginFolder, $"{assemblyName.Name}.dll");
+            if (File.Exists(assemblyPath))
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+            return null;
+        }
+    }
+
+}
