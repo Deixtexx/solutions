ab8a0eb806ccdd02e6d7ca90c9c1b49966b6073f
Deixtexx <serebrovdaniil@gmail.com>
2026-07-16 19:09:04 +0600

    задание 14

---
+++ practice2026_3/task14tests/DefiniteIntegralTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using task14;
+
+namespace task14tests
+{
+    public class DefiniteIntegralTests
+    {
+        [Fact]
+        public void DefiniteIntegral_CorrectValueOnLinearFunction()
+        {
+            var X = (double x) => x;
+            Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+        }
+
+        [Fact]
+        public void DefiniteIntegral_CorrectValueOnSin()
+        {
+            var SIN = (double x) => Math.Sin(x);
+            Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+        }
+
+        [Fact]
+        public void DefiniteIntegral_CorrectValueOnXInPositiveRange()
+        {
+            var X = (double x) => x;
+            Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+        }
+    }
+}
