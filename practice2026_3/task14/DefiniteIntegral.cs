14e1cf40534f945d1fdc35292e3aad08612ae5e0
Deixtexx <serebrovdaniil@gmail.com>
2026-07-16 20:40:34 +0600

    задание 15

--- practice2026_3/task14/DefiniteIntegral.cs
+++ practice2026_3/task14/DefiniteIntegral.cs
@@ -2,47 +2,61 @@
 {
     public class DefiniteIntegral
     {
-        public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+        public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
         {
-            int totalSteps = (int)((b - a) / step);
+            int totalSteps = (int) Math.Round((b - a) / step);
             step = (b - a) / totalSteps;
 
-            int stepsPerThread = totalSteps / threadsnumber;
+            int stepsPerThread = totalSteps / threadsNumber;
             double result = 0.0;
 
-            using (Barrier barrier = new Barrier(threadsnumber + 1))
+            using (CountdownEvent countdown = new CountdownEvent(threadsNumber))
             {
-                for (int i = 0; i < threadsnumber; i++)
+                for (int i = 0; i < threadsNumber; i++)
                 {
                     int threadIndex = i;
                     ThreadPool.QueueUserWorkItem(_ =>
                     {
                         int startStep = threadIndex * stepsPerThread;
-                        int endStep = (threadIndex == threadsnumber - 1) ? totalSteps : (threadIndex + 1) * stepsPerThread;
+                        int endStep = (threadIndex == threadsNumber - 1) ? totalSteps : (threadIndex + 1) * stepsPerThread;
 
-                        // Вычисляем физические границы отрезка для текущего потока заранее
-                        double threadA = a + startStep * step;
                         double localSum = 0.0;
+                        double threadA = a + startStep * step;
 
+                        double prevY = function(threadA);
                         for (int j = 0; j < (endStep - startStep); j++)
                         {
-                            // Считаем точки относительно начала отрезка потока, это снижает погрешность double
-                            double x1 = threadA + j * step;
-                            double x2 = threadA + (j + 1) * step;
-
-                            localSum += (function(x1) + function(x2)) * step / 2.0;
+                            double nextX = threadA + (j + 1) * step;
+                            double nextY = function(nextX);
+                            localSum += (prevY + nextY);
+                            prevY = nextY;
                         }
+                        localSum = localSum * step / 2.0;
 
                         AddDouble(ref result, localSum);
-                        barrier.SignalAndWait();
-                    });
 
+                        countdown.Signal();
+                    });
                 }
 
-                barrier.SignalAndWait();
+                countdown.Wait();
             }
-
             return result;
+        }
+
+        public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+        {
+            int totalSteps = (int) Math.Round((b - a) / step);
+            step = (b - a) / totalSteps;
+            double sum = 0.0;
+
+            for (int i = 0; i < totalSteps; i++)
+            {
+                double x1 = a + i * step;
+                double x2 = a + (i + 1) * step;
+                sum += (function(x1) + function(x2)) * step / 2.0;
+            }
+            return sum;
         }
 
         private static void AddDouble(ref double result, double value)
